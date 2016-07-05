87710e3f69f140ee2f5f579deca8c6383a89c50c
Daniele Monteleone <daniele.monteleone.it@gmail.com>
2016-07-05 22:12:53 +0200

    now listening for activate event

--- app/sw.js
+++ app/sw.js
@@ -1,11 +1,16 @@
 self.addEventListener('install', e => e.waitUntil(
-    caches.open('espapp').then(cache => cache.addAll([
+    caches.open('espapp')
+    .then(cache => cache.addAll([
         '/',
         '/index.html'
     ]))
 ));
+self.addEventListener('activate', e => {
+    e.waitUntil(self.clients.claim());
+});
 self.addEventListener('fetch', e => {
-    e.respondWith(caches.match(e.request).then(r => {
-        return r || fetch(e.request);
-    }));
+    e.respondWith(caches.match(e.request)
+        .then(r => {
+            return r || fetch(e.request);
+        }));
 });
