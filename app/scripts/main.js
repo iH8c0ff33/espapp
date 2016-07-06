cecdbe1fd0c29ec25d23eb1450481f0d17ded361
Daniele Monteleone <daniele.monteleone.it@gmail.com>
2016-07-06 11:54:43 +0200

    added material design lite

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,3 +3,9 @@ if ('serviceWorker' in navigator) {
         .register('/sw.js')
         .then(() => console.log('sw.js registered'));
 }
+var button = document.createElement('button');
+var textNode = document.createTextNode('Click Me!');
+button.appendChild(textNode);
+button.className = 'mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent';
+componentHandler.upgradeElement(button);
+document.getElementById('container').appendChild(button);
--- gulpfile.js
+++ gulpfile.js
@@ -6,26 +6,34 @@ let uglify = require('uglify-js-harmony');
 let csso = require('csso');
 let fs = require('fs');
 
-gulp.task('copy', function () {
+gulp.task('root', function () {
     gulp.src([
         'app/*',
         '!app/*.html'
-    ], {
-        dot: true
-    })
+    ])
         .pipe(gulp.dest('dist'))
         .pipe($.size({
-            title: 'copy'
+            title: 'root'
         }));
 });
 
-gulp.task('html', ['copy'], function () {
+gulp.task('icons', function () {
+    gulp.src(['app/fonts/*'])
+    .pipe(gulp.dest('dist/fonts'))
+    .pipe($.size({
+        title: 'icons'
+    }));
+});
+
+gulp.task('html', ['root', 'icons'], function () {
     return gulp.src('app/**/*.html')
         .pipe($.replace(/<script src="([^\.]*\.js)"[^>]*>[^<]*<\/script>/g, (s, file) => {
             return '<script>' + uglify.minify('app' + file).code + '</script>';
         }))
         .pipe($.replace(/<link rel="stylesheet" href="([^\.]*\.css)"[^>]*>/g, (s, file) => {
-            return '<style>' + csso.minify(fs.readFileSync('app' + file, 'utf8')).css + '</style>';
+            return '<style>' + csso.minify(fs.readFileSync('app' + file, 'utf8'), {
+                comments: false
+            }).css + '</style>';
         }))
         .pipe($.htmlmin({
             collapseWhitespace: true,
